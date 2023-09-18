78900db36948911a466f22b8072eab9491af6fca
Moaad Amer <moaad02@gmail.com>
2023-09-18 15:04:15 +0300

    Geneate infinity number of colors and show the value on the screen

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-let index = 0;
 
 function changeColor() {
 
-    let colors = ["red", "blue", "orange", "yellow", "green", "purple"];
-
     let body = document.getElementsByTagName("body")[0];
 
-    body.style.background = colors[index++];
+    const r = Math.floor(Math.random() * 255);
+    const g = Math.floor(Math.random() * 255);
+    const b = Math.floor(Math.random() * 255);
 
-    index = index % colors.length;
+    body.style.background = `rgb(${r},${g},${b})`;
+
+    let h1 = document.getElementsByTagName("h1")[0];
+
+    h1.innerText=`rgb(${r},${g},${b})`;
+
 }
